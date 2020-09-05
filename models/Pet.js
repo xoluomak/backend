a3de2bfb5c5c439dff68a5850299769b3be6fb0c
kamoulox <yoann.cayron@epitech.eu>
2020-09-05 13:45:19 +0200

    lastMeal

--- models/Pet.js
+++ models/Pet.js
@@ -23,6 +23,7 @@ const petSchema = mongoose.Schema({
     reptile: { type: reptileSchema, required: true },
     birthday: { type: Date, required: false },
     shade: { type: [Date], required: false },
+    lastMeal: { type: Date, required: false },
     calciumFreq: { type: Number, required: false },
     vitaminFreq: { type: Number, required: false }
   });
