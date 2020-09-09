b68fd6d5773e3c316caefba7dd8c78e86d313765
kamoulox <yoann.cayron@epitech.eu>
2020-09-09 17:02:08 +0200

    save

--- models/Pet.js
+++ models/Pet.js
@@ -15,7 +15,7 @@ const reptileSchema = mongoose.Schema({
   imageUrl: { type: String, required: false },
   group: { type: String, required: true },
   foodType: { type: [String], required: true },
-  frequency: { type: [Schema.Types.ObjectId], required: true, ref: frequencySchema },
+  frequency: { type: [Schema.Types.ObjectId], ref: 'Frequency' },
 });
    
 const lastMealSchema = mongoose.Schema({
@@ -28,15 +28,19 @@ const petSchema = mongoose.Schema({
   name: { type: String, required: true },
   owner: { type: String, required: true },
   enabled: { type: Boolean, required: true },
-  reptile: { type: Schema.Types.ObjectId, required: true, unique: false, ref: reptileSchema },
+  reptile: { type: Schema.Types.ObjectId, ref: 'Reptile' },
   birthday: { type: Date, required: false },
   shade: { type: [Date], required: false },
-  lastMeal: { type: [Schema.Types.ObjectId], required: false, ref: lastMealSchema },
+  lastMeal: { type: [Schema.Types.ObjectId], ref: 'LastMeal' },
   calciumFreq: { type: Number, required: false },
   vitaminFreq: { type: Number, required: false }
 });
 
 reptileSchema.plugin(uniqueValidator);
 petSchema.plugin(uniqueValidator);
+
+const Reptile = mongoose.model('Reptile', reptileSchema);
+const LastMeal = mongoose.model('LastMeal', lastMealSchema);
+const Frequency = mongoose.model('Frequency', frequencySchema);
 
 module.exports = mongoose.model('Pet', petSchema);
