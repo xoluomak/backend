785332774449c766d599357ca61fd7867a85d874
kamoulox <yoann.cayron@epitech.eu>
2020-09-09 18:59:03 +0200

    save

--- models/Pet.js
+++ models/Pet.js
@@ -1,21 +1,4 @@
 const mongoose = require('mongoose');
-
-const frequencySchema = mongoose.Schema({
-  period: { type: String, required: true },
-  periodStart: { type: Number, required: true },
-  periodEnd: { type: Number, required: false },
-  foodType: { type: String, required: true },
-  number: { type: String, required: true },
-  delay: { type: String, required: true },
-});
-   
-const reptileSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  imageUrl: { type: String, required: false },
-  group: { type: String, required: true },
-  foodType: { type: [String], required: true },
-  frequency: { type: [frequencySchema], required: true },
-});
    
 const lastMealSchema = mongoose.Schema({
   date: { type: Date, required: true },
@@ -27,7 +10,7 @@ const petSchema = mongoose.Schema({
   name: { type: String, required: true },
   owner: { type: String, required: true },
   enabled: { type: Boolean, required: true },
-  reptile: { type: reptileSchema, required: true },
+  reptileId: { type: String, required: true },
   birthday: { type: Date, required: false },
   shade: { type: [Date], required: false },
   lastMeal: { type: [lastMealSchema], required: false },
