2a98e035959d1efc39c51b3b89165a28420ce896
kamoulox <yoann.cayron@epitech.eu>
2020-11-10 11:52:27 +0100

    save

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const flowerRoutes = require('./routes/flower');
 const path = require('path');
 
 const app = express();
-
-  mongoose.connect('mongodb+srv://admin:admin1234@realmcluster.mgck9.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  mongoose.connect('mongodb+srv://admin:admin1234@realmcluster.mgck9.mongodb.net/repminder?retryWrites=true&w=majority',
     { useNewUrlParser: true,
         useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected !'))
